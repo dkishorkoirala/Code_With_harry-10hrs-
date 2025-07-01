d02c8964c87be0c5b6c16aa8aaff3a2128539e9d
dkishorkoirala <reyzual.info@gmail.com>
2025-07-01 17:10:28 +0545

    chapter 1 of code with harry modules and comments

---
+++ chapter1/ch-1 ps/problem4.py
@@ -0,0 +1,5 @@
+import os
+
+#this creates the for loop with os.listdir('/')
+for item in os.listdir('/'):
+    print(item)
---
+++ chapter1/module.py
@@ -0,0 +1,13 @@
+import pyjokes
+
+
+# print("Printing jokes...")
+
+# this prints a random jokes
+joke = pyjokes.get_joke()
+print(joke)
+"""
+thank you for watching
+and lover you more
+okay
+"""
