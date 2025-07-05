5641b7b82241bba2930fed84ad71614aa129fa02
dkishorkoirala <reyzual.info@gmail.com>
2025-07-05 15:47:14 +0545

    here is the chapter 6 conditionals

---
+++ chapter6 conditional expression/Practice set CH-6/07_problem7.py
@@ -0,0 +1,7 @@
+post = input("Enter the post: ")
+
+if ("Dibash".lower() in post.lower()):
+    print("This post is talking about Dibash")
+
+else:
+    print("This post is not talking about Dibash")
